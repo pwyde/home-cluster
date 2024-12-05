dc4a95f1da9ff4f08b5a2171fced632ed4a5dbb8
Patrik Wyde <patrik@wyde.se>
2024-12-05 10:20:32 +0100

    fix(kubeconform): skip ExternalSecret

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -12,7 +12,7 @@ kubeconform_args=(
     "-strict"
     "-ignore-missing-schemas"
     "-skip"
-    "Secret,ReplicationSource,ReplicationDestination"
+    "ExternalSecret,Secret,ReplicationSource,ReplicationDestination"
     "-schema-location"
     "default"
     "-schema-location"
